e1a35fe6709e15192629a45b495ff91911f5fb9a
Chase Wesley <chasewesley9451@gmail.com>
2020-03-08 16:17:00 -0400

    Added jQuery and refactor

---
+++ page/tab.js
@@ -0,0 +1,20 @@
+const defaultTab = createElement('tab.html');
+
+function createTab(tab)
+{
+    let newTab = defaultTab.cloneNode(true);
+
+    findFirstChildByClass(newTab, 'tab-title').innerHTML = tab.title;
+    findFirstChildByClass(newTab, 'tab-url').innerHTML = tab.url;
+    findFirstChildByClass(newTab, 'tab-link').href = tab.url;
+    findFirstChildByClass(newTab, 'tab-icon').src = tab.iconUrl;
+
+    var drag = findFirstChildByClass(newTab, "tab-drag");
+
+    $(drag).click(() =>
+    {
+        $(drag).toggleClass('tab-drag-down');
+    });
+
+    return newTab;
+}
